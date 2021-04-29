52154d0e237651ffa4dd9662a2b8ae0cea249367
Neshoz <kevin.nemec94@gmail.com>
2021-04-29 21:30:57 +0200

    Show the difference between code splitting and not

--- packages/hub/src/campaign.tsx
+++ packages/hub/src/campaign.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useEntity } from '@mediatool-poc/tools'
-import { IParams } from './types'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useEntity } from "@mediatool-poc/tools";
+import { IParams } from "./types";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const Campaign = () => {
-  const { campaignId } = useParams<IParams>()
-  
-  const { data } = useEntity('campaign', campaignId)
+  const { campaignId } = useParams<IParams>();
 
-  console.log(data)
+  const { data } = useEntity("campaign", campaignId);
+
+  console.log(data);
 
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <p>This is actually working</p>
       <p>{JSON.stringify(data, null, 2)}</p>
-    </>
-  )
-}
+    </DndProvider>
+  );
+};
 
-export default Campaign
+export default Campaign;
--- packages/hub/src/index.tsx
+++ packages/hub/src/index.tsx
@@ -1,40 +1,35 @@
-
-import React from 'react'
-import { Route, Switch, useHistory } from 'react-router-dom'
-import { LazyPage, useEntity } from '@mediatool-poc/tools'
-import { id } from './util'
-import { ICampaign } from './types'
+import React from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { LazyPage, useEntity } from "@mediatool-poc/tools";
+import { id } from "./util";
+import { ICampaign } from "./types";
+// import Campaign from "./campaign";
 
 const Hub = () => {
-  const history = useHistory()
-  const {
-    createEntity,
-  } = useEntity('campaign')
+  const history = useHistory();
+  const { createEntity } = useEntity("campaign");
 
   const handleClick = () => {
-    const _id = id()
-    createEntity<ICampaign>({ _id, name: 'Created test campaign' })
-    history.push(`/hub/${_id}`)
-  }
+    const _id = id();
+    createEntity<ICampaign>({ _id, name: "Created test campaign" });
+    history.push(`/hub/${_id}`);
+  };
 
   return (
     <>
       <p>This is hub</p>
       <button onClick={handleClick}>Create entity</button>
-        <Switch>
-          <Route
-            exact
-            path="/hub/:campaignId"
-            render={props => (
-              <LazyPage
-                { ...props }
-                loader={() => import('./campaign')}
-              />
-            )}
-          />
-        </Switch>
+      <Switch>
+        <Route
+          exact
+          path="/hub/:campaignId"
+          render={(props) => (
+            <LazyPage {...props} loader={() => import("./campaign")} />
+          )}
+        />
+      </Switch>
     </>
-  )
-}
+  );
+};
 
-export default Hub
+export default Hub;
