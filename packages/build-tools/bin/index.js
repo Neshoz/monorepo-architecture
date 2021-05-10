b41ca982055f74fce068d059baa3441392913668
Neshoz <kevin.nemec94@gmail.com>
2021-05-10 22:03:50 +0200

    Added external dependencies to esbuild

--- packages/build-tools/bin/index.js
+++ packages/build-tools/bin/index.js
@@ -5,35 +5,57 @@ const path = require('path')
 const program = require('commander')
 const { exec } = require('child_process')
 
+function featurePath(feature) {
+  return path.join(__dirname, '../../', feature)
+}
+
+function getPkg(pkg) {
+  return require(path.join(featurePath(pkg), 'package.json'))
+}
+
 program
   .command('build')
   .description('This is a test build')
   .requiredOption('-f, --feature [value]', 'Which feature to build')
   .option('-e, --env [value]', 'Environment to create build for', 'dev')
   .action((args) => {
-    const featurePath = path.join(__dirname, '../../', args.feature)
-    exec(`rimraf ${featurePath}/dist`, (err) => {
+    const corePkg = getPkg('core')
+    const featurePkg = getPkg(args.feature)
+    const fPath = featurePath(args.feature)
+
+    exec(`rimraf ${fPath}/dist`, (err) => {
       if (err) {
         console.error(err)
         process.exit(1)
       }
     })
-    exec(`tsc --project ${featurePath} --declaration --emitDeclarationOnly`, (typeCheckErr) => {
+    exec(`tsc --project ${fPath} --declaration --emitDeclarationOnly`, (typeCheckErr) => {
       if (typeCheckErr) {
         console.error(typeCheckErr)
         process.exit(1)
       }
 
+      const external = [...new Set([
+        ...Object.keys(corePkg.dependencies),
+        ...Object.keys(featurePkg.dependencies)
+      ])].filter(str => !str.includes('@mediatool'))
+
+      console.log(external)
+
       build({
-        entryPoints: [`${featurePath}/src/index.tsx`],
+        entryPoints: [`${fPath}/src/index.tsx`],
         chunkNames: '[name]-[hash]',
-        outdir: `${featurePath}/dist`,
+        outdir: `${fPath}/dist`,
         bundle: true,
         splitting: true,
         format: 'esm',
         minify: args.env === 'prod',
         sourcemap: args.env === 'dev',
-        external: ['react', 'react-router-dom', 'react-redux', 'redux', '@chakra-ui/react']
+        external: [
+          'react',
+          'react-router-dom',
+          ...external
+        ]
       }).catch((err) => {
         console.error(err)
         process.exit(1)
